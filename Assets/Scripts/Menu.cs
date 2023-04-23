0b7475c4957880c8a3be76cac1de5d9120fe7b1d
COPP-CC-19\ocenka <agaki08@internet.ru>
2023-04-23 16:49:25 +0300

    Added Menu, Character controller, 1st location(barriers, palmtrees, coconuts), Sprites( barrels, pipes)

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject settingsmenu;
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SettingsMenu()
+    {
+        menu.SetActive(false);
+        settingsmenu.SetActive(true);
+    }
+
+    public void Back()
+    {
+        settingsmenu.SetActive(false);
+        menu.SetActive(true);
+    }
+}
