98f81ffc78576ca80d418a251560a02a6898a30d
RICCOs <agaki08@internet.ru>
2023-05-05 23:32:43 +0300

    Added Changing Scenes

--- Assets/Scripts/TriggerForLoadScene.cs
+++ Assets/Scripts/TriggerForLoadScene.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TriggerForLoadScene : MonoBehaviour
 {
+    [SerializeField] private GameObject _textGo;
+    [SerializeField] private int _indexScene;
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.name);
+        Debug.Log(other.name + " in Trigger...");
+        _textGo.SetActive(true);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" & Input.GetKeyDown(KeyCode.N))
+        {
+            SceneManager.LoadScene(_indexScene);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _textGo.SetActive(false);
     }
 }
