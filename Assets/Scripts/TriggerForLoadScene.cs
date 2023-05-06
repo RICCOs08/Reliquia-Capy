0ccd6f049f636bdce04744c4347407ff948de54b
COPP-CC-19\ocenka <agaki08@internet.ru>
2023-05-06 18:17:22 +0300

    Fixed MenuPause, Changing Scenes

--- Assets/Scripts/TriggerForLoadScene.cs
+++ Assets/Scripts/TriggerForLoadScene.cs
@@ -7,15 +7,21 @@ public class TriggerForLoadScene : MonoBehaviour
 {
     [SerializeField] private GameObject _textGo;
     [SerializeField] private int _indexScene;
+    private bool _isActiveTrigger;
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.name + " in Trigger...");
-        _textGo.SetActive(true);
+        if (other.tag == "Player")
+        {
+            Debug.Log(other.name + " in Trigger...");
+            _textGo.SetActive(true);
+            _isActiveTrigger = true;
+        }
+            
     }
 
-    private void OnTriggerStay(Collider other)
+    private void Update()
     {
-        if (other.tag == "Player" & Input.GetKeyDown(KeyCode.N))
+        if (_isActiveTrigger && Input.GetKeyDown(KeyCode.N))
         {
             SceneManager.LoadScene(_indexScene);
         }
@@ -23,6 +29,10 @@ public class TriggerForLoadScene : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        _textGo.SetActive(false);
+        if (other.tag == "Player")
+        {
+            _textGo.SetActive(false);
+            _isActiveTrigger = false;   
+        }
     }
 }
